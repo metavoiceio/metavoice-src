1eb79fab44e520d5999598559817fdb761b00eb0
Vatsal Aggarwal <vatsal@themetavoice.xyz>
2024-03-15 09:58:32 +0000

    fix pr comments

--- fam/llm/fast_quantize.py
+++ fam/llm/fast_quantize.py
@@ -48,12 +48,10 @@ def dynamically_quantize_per_channel(x, quant_min, quant_max, target_dtype):
     min_val, max_val = torch.aminmax(x, dim=1)
 
     # calculate scales and zero_points based on min and max
-    # reference: https://fburl.com/code/srbiybme
     min_val_neg = torch.min(min_val, torch.zeros_like(min_val))
     max_val_pos = torch.max(max_val, torch.zeros_like(max_val))
     device = min_val_neg.device
 
-    # reference: https://fburl.com/code/4wll53rk
     max_val_pos = torch.max(-min_val_neg, max_val_pos)
     scales = max_val_pos / (float(quant_max - quant_min) / 2)
     # ensure scales is the same dtype as the original tensor
@@ -61,7 +59,6 @@ def dynamically_quantize_per_channel(x, quant_min, quant_max, target_dtype):
     zero_points = torch.zeros(min_val_neg.size(), dtype=torch.int64, device=device)
 
     # quantize based on qmin/qmax/scales/zp
-    # reference: https://www.internalfb.com/code/fbsource/[8edc275012b1]/fbcode/caffe2/torch/ao/quantization/fx/_decomposed.py?lines=63
     x_div = x / scales.unsqueeze(-1)
     x_round = torch.round(x_div)
     x_zp = x_round + zero_points.unsqueeze(-1)
